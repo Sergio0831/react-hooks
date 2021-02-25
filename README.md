df4c4e9f63bdd58d7dc025e88518ff65ebbeff85
Sergejs Ivcenko <sergejs.ivcenko@gmail.com>
2021-02-25 10:10:56 +0000

    added useMemo() state

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## 2. useEffect()
 
 ## 3. useRef()
+
+## 4. useMemo()
--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import './App.css';
 
+function complexCompute(num) {
+	let i = 0;
+	while (i < 10000000) i++;
+	return num * 2;
+}
+
 function App() {
-	// const [renderCount, setRenderCount] = useState(1);
-	const [value, setValue] = useState('initial');
-	const renderCount = useRef(1);
-	const inputRef = useRef(null);
-	const prevValue = useRef('');
+	const [number, setNumber] = useState(42);
+	const [colored, setColored] = useState(false);
+
+	const styles = useMemo(
+		() => ({
+			color: colored ? 'darkred' : 'black',
+		}),
+		[colored]
+	);
+
+	const computed = useMemo(() => {
+		return complexCompute(number);
+	}, [number]);
 
 	useEffect(() => {
-		renderCount.current++;
-	});
-
-	const focus = () => inputRef.current.focus();
-
-	useEffect(() => {
-		prevValue.current = value;
-	}, [value]);
+		console.log('style changed');
+	}, [styles]);
 
 	return (
 		<div className="App">
-			<h2>Render qty: {renderCount.current}</h2>
-			<h2>Prev value: {prevValue.current}</h2>
-			<input
-				type="text"
-				ref={inputRef}
-				onChange={(e) => setValue(e.target.value)}
-			/>
-			<button className="btn btn-success m-1" onClick={focus}>
-				Focus
+			<h1 style={styles}>Property: {number}</h1>
+			<button
+				className="btn btn-success m-1"
+				onClick={() => setNumber((prev) => prev + 1)}
+			>
+				Increment
+			</button>
+			<button
+				className="btn btn-danger m-1"
+				onClick={() => setNumber((prev) => prev + 1)}
+			>
+				Decrement
+			</button>
+			<button
+				className="btn btn-warning m-1"
+				onClick={() => setColored((prev) => !prev)}
+			>
+				Change
 			</button>
 		</div>
 	);
